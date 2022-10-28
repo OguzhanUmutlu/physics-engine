66817597f9754ea01c3e734ecdb20beb9b43f3a6
Oğuzhan <oguzhan.umutlu123@gmail.com>
2022-10-28 23:50:43 +0300

    fixed a lot of things and made the basic mechanics

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Haven't done much.
 
 Used lots of trig functions and "Newton"s equations
 
-[Click to view preview](https://oguzhanumutlu.github.io/physics-engine/legacy)
+[Click to view old preview](https://oguzhanumutlu.github.io/physics-engine/legacy)
+
+[Click to view new preview](https://oguzhanumutlu.github.io/physics-engine)
--- engine.js
+++ engine.js
@@ -3,6 +3,7 @@
     const _global = isWeb ? window : global;
     const _exports = isWeb ? window : module.exports;
     let _p;
+    let _id = 0;
     _exports.Phygic = new Promise(r => _p = r);
     const Collider2D = isWeb ? await (async function () {
         if (typeof window.Collider2D !== "undefined") return Collider2D;
@@ -20,7 +21,23 @@
         if (isWeb) cancelAnimationFrame(f);
     };
     const collider = new Collider2D.Collider2D();
-    const {PI, floor, ceil, round, sqrt, pow, sin, cos, tan, abs} = Math;
+    const {PI, floor, ceil, round, sqrt, pow, sin, cos, tan, abs, atan, atan2} = Math;
+
+    function processOptions(options, def, mode = 0) {
+        if (typeof options !== "object" || Array.isArray(options)) options = {};
+        switch (mode) {
+            case 0: // overwrite mode
+                Object.keys(def).forEach(i => typeof options[i] !== typeof def[i] && (options[i] = def[i]));
+                break;
+            case 1: // exists mode
+                Object.keys(def).forEach(i => !Object.keys(options).includes(i) && (options[i] = def[i]))
+                break;
+            case 2: // deep mode
+                def.forEach(i => typeof eval(`options.${i[0]}`) !== typeof i[1] && eval(`options.${i[0]} = i[1]`));
+                break;
+        }
+        return options;
+    }
 
     function extendClass(self, extend) {
         Object.keys(extend.prototype).forEach(f => self.prototype[f] = extend.prototype[f]);
@@ -82,12 +99,13 @@
     }
 
     Circle.prototype.draw = function (world, tile) {
+        const t = world.translation;
         const ctx = world.ctx;
         ctx.save();
         ctx.fillStyle = this.fill;
         ctx.strokeStyle = this.stroke;
         ctx.beginPath();
-        ctx.arc(tile.x, tile.y, this.radius, 0, PI * 2);
+        ctx.arc(t.x + tile.x, t.y + tile.y, this.radius, 0, PI * 2);
         if (this.fill) ctx.fill();
         if (this.stroke) ctx.stroke();
         ctx.closePath();
@@ -104,12 +122,13 @@
                 path = v;
                 this._hasChanged = true;
             }
-        })
+        });
     }
 
     Polygon.prototype.draw = function (world, tile) {
         const ctx = world.ctx;
         ctx.save();
+        ctx.translate(world.translation.x, world.translation.y);
         ctx.fillStyle = this.fill;
         ctx.strokeStyle = this.stroke;
         /*ctx.translate(tile.x, tile.y);
@@ -128,13 +147,16 @@
             });
         }
         ctx.beginPath();
+        ctx.scale(world.scale, world.scale);
         (this._lastPoints || this.path).forEach((i, j) => ctx[j === 0 ? "moveTo" : "lineTo"](tile.x + i[0], tile.y + i[1]));
         if (this.fill) ctx.fill();
         if (this.stroke) ctx.stroke();
         ctx.closePath();
-        ctx.fillStyle = "#00ff00";
-        this._lastPoints.forEach(i => ctx.fillRect(tile.x + i[0] - 2, tile.y + i[1] - 2, 4, 4));
-        ctx.fillRect(tile.x - 2, tile.y - 2, 4, 4);
+        const rnd = world.options.render;
+        const rndCls = rnd.collisions;
+        ctx.fillStyle = rndCls.color;
+        (this._lastPoints || this.path).forEach(i => ctx.fillRect(tile.x + i.y - rndCls.size / 2, tile.y + i.y - rndCls.size / 2, rndCls.size, rndCls.size));
+        ctx.fillRect(tile.x - rndCls.size / 2, tile.y - rndCls.size / 2, rndCls.size, rndCls.size);
         ctx.restore();
     };
 
@@ -158,7 +180,7 @@
                     this.setSize(width, height);
                 }
             }
-        })
+        });
     }
 
     Rectangle.prototype.setSize = function (width, height) {
@@ -172,27 +194,52 @@
         ];
     };
 
+    const tiles = {};
+
     function Tile(x = 0, y = 0, opts) {
         Vector2.apply(this, arguments);
-        if (!opts || typeof opts !== "object" || Array.isArray(opts)) opts = {};
-        const def = {
-            shape: null, rotation: 0, gravityEnabled: true, world: null, mass: 1
-        };
-        Object.keys(def).forEach(i => !Object.keys(opts).includes(i) && (opts[i] = def[i]));
+        opts = processOptions(opts, {
+            shape: null, rotation: 0, gravityEnabled: true, world: null, mass: 10,
+            staticFrictionCoefficient: 0, kineticFrictionCoefficient: 0
+        }, 1);
+        const id = _id++;
+        tiles[id] = this;
+        Object.defineProperty(this, "id", {
+            get: () => id
+        });
         this.shape = opts.shape;
         this.rotation = opts.rotation;
         this.rotationTarget = opts.rotation;
+        this.gravitationalVelocity = 0;
+        this.horizontalVelocity = 0;
         this.velocity = Vector2.zero();
         this.force = Vector2.zero();
         this.motion = Vector2.zero();
         this.mass = opts.mass;
         this.gravityEnabled = opts.gravityEnabled;
         this.dragCoefficient = 1;
+        this.outOfDistance = false;
         if (opts.world instanceof World) opts.world.addTile(this);
         this.initOptions = opts;
         Object.freeze(this.initOptions);
     }
 
+    Tile.getById = function (id) {
+        return tiles[id];
+    };
+    Tile.prototype.getRadius = function () {
+        switch (this.shape?.type) {
+            case "polygon":
+            case "rectangle":
+                const a = this.shape.path.map(i => i[0]);
+                const w = a.sort((a, b) => b - a)[0] - a.sort((a, b) => a - b)[0];
+                const b = this.shape.path.map(i => i[1]);
+                const h = b.sort((a, b) => b - a)[0] - b.sort((a, b) => a - b)[0];
+                return sqrt(w ** 2 + h ** 2);
+            case "circle":
+                return this.shape.radius;
+        }
+    };
     Tile.prototype.getBottomArea = function () {
         switch (this.shape?.type) {
             case "polygon":
@@ -213,16 +260,12 @@
         return new Vector2(cos(this.rotation), -sin(this.rotation));
     };
     Tile.prototype.getCollider = function () {
-        const angle = t => {
-            t.setAngle(this.rotation);
-            return t;
-        };
         switch (this.shape?.type) {
             case "circle":
-                return ["circle", angle(new Collider2D.Circle(new Collider2D.Vector(this.x, this.y), this.shape.radius))];
+                return ["circle", new Collider2D.Circle(new Collider2D.Vector(this.x, this.y), this.shape.radius)];
             case "polygon":
             case "rectangle":
-                return ["polygon", angle(new Collider2D.Polygon(new Collider2D.Vector(this.x, this.y), (this.shape._lastPoints || this.shape.path).map(i => new Collider2D.Vector(...i))))];
+                return ["polygon", new Collider2D.Polygon(new Collider2D.Vector(this.x, this.y), (this.shape._lastPoints || this.shape.path).map(i => new Collider2D.Vector(i[0], i[1])))];
             default:
                 return null;
         }
@@ -273,11 +316,30 @@
                 return false;
         }
     };
-    Tile.prototype.move = function (world, vec, f = null) {
-        this.set(this.add(vec));
-        const collision = Array.from(world.tiles).find(i => i !== this && i.collidesWith(this));
+    Tile.prototype.getCollisions = function (world) {
+        return Array.from(world.tiles).find(i => i !== this && i.collidesWith(this));
+    };
+    Tile.prototype.move = function (world, vec) {
+        //if (!this.gravityEnabled) return false;
+        let collision;
+        collision = this.getCollisions(world);
+        if (collision) return false;
+        let back;
+        const start = this.clone();
+        // let's ray-cast?
+        const lRotation = atan2(vec.x, vec.y);
+        //const radius = this.getRadius();
+        const moveVec = new Vector2(sin(lRotation), cos(lRotation));//.scale(radius/2);
+        const dist = Math.min(ceil(vec.len() / moveVec.len()), world.maxRayCastingIterations);
+        for (let i = 0; i < dist; i++) {
+            back = this.clone();
+            if (i === dist - 1) this.set(start.add(vec));
+            else this.set(this.add(moveVec));
+            collision = this.getCollisions(world);
+            if (collision) break;
+        }
         if (collision) {
-            if (collision.shape && collision.shape.type === "polygon") {
+            if (collision.shape && (collision.shape.type === "polygon" || collision.shape.type === "rectangle")) {
                 const nearest = collision.shape.path.map((i, j) => {
                     const next = collision.shape.path[collision.shape.path.length === j + 1 ? 0 : j + 1];
                     return new Collider2D.Polygon(new Collider2D.Vector(collision.x, collision.y), [
@@ -297,37 +359,65 @@
                     return [a, dist];
                 }).sort((a, b) => a[1] - b[1])[0];
                 if (nearest) {
-                    this.rotationTarget = -Math.atan(nearest[0]);
+                    if (abs(nearest[0]) === Infinity) nearest[0] = 0;
+                    this.rotationTarget = -atan(nearest[0]);
                     if (this.rotationTarget === 90) this.rotationTarget = 0;
                 }
             }
-            this.set(this.sub(vec));
+            this.set(back);
             return false;
         }
         return true;
     };
     Tile.prototype.update = function (world, deltaTime) {
         if (!deltaTime) return;
-        this.velocity.set(this.velocity.add(this.force));
-        const terminalVelocity = sqrt(2 * this.mass * world.gravityAcceleration / (world.fluidDensity * this.getBottomArea() * this.dragCoefficient));
-        if (this.gravityEnabled) this.velocity.y += this.mass * world.gravityAcceleration / deltaTime;
+        if (this.dist(world.translation) > sqrt((world.canvas.width * (1 / world.scale)) ** 2 + (world.canvas.height * (1 / world.scale)) ** 2) / 2 + world.updateDistance) return this.outOfDistance = true;
+        this.outOfDistance = false;
+        if (deltaTime > 100) console.warn(deltaTime);
+        // NOTE: I got the square root of the bottom area(not sure what I could have done)
+        const terminalVelocity = sqrt(2 * this.mass * world.gravityAcceleration / (world.fluidDensity * sqrt(this.getBottomArea()) * this.dragCoefficient));
         //if (this.velocity.y > terminalVelocity) this.velocity.y = terminalVelocity;
-        if (!this.move(world, {x: this.velocity.x, y: 0})) this.velocity.x /= 2;
-        if (!this.move(world, {x: 0, y: this.velocity.y})) this.velocity.y /= 2;
-        if (this.move(world, {x: this.motion.x, y: this.motion.y})) this.motion.set(this.motion.scale(9 / 10))
-        this.rotation += (this.rotationTarget - this.rotation) / 5;
+        if (this.gravityEnabled) {
+            this.gravitationalVelocity += this.mass * world.gravityAcceleration * deltaTime / 1000;
+            if (!this.move(world, new Vector2(0, this.gravitationalVelocity))) this.gravitationalVelocity = 0;
+            this.horizontalVelocity += this.mass * world.gravityAcceleration * deltaTime / 1000 * -sin(this.rotation);
+            console.log()
+            if (!this.move(world, new Vector2(Math.cos(this.rotation) * this.horizontalVelocity, Math.sin(this.rotation) * this.horizontalVelocity))) this.horizontalVelocity = 0;
+        }
+        if (round(this.motion.x) !== 0 && round(this.motion.y) !== 0) this.move(world, new Vector2(this.motion.x / 10, this.motion.y / 10));
+        this.motion.set(this.motion.scale(9 / 10));
+        this.rotation += (this.rotationTarget - this.rotation) / 2;
+    };
+    Tile.prototype.resetVelocities = function () {
+        this.gravitationalVelocity = 0;
+        this.horizontalVelocity = 0;
+        this.velocity.set(Vector2.zero());
+        this.motion.set(Vector2.zero());
     };
 
     const worlds = {};
-    let _w_id = 0;
 
-    function World(canvas, ctx) {
+    function World(canvas, options) {
+        options = processOptions(options, [
+            ["render", {}],
+            ["render.collisions", {}],
+            ["render.collisions.color", ""],
+            ["render.collisions.size", 4],
+            ["scale", 1],
+            ["maxRayCastingIterations", 1000],
+            ["updateDistance", 10000],
+        ], 2);
+        this.options = options;
+        this.scale = options.scale;
+        this.translation = Vector2.zero();
         this.tiles = new Set;
         this.canvas = canvas;
-        this.ctx = canvas.getContext ? canvas.getContext("2d") : ctx;
+        this.ctx = canvas.getContext ? canvas.getContext("2d") : options.ctx;
         this.gravityAcceleration = 2; // px / second
         this.fluidDensity = 1.225;
-        const id = _w_id++;
+        this.maxRayCastingIterations = options.maxRayCastingIterations;
+        this.updateDistance = options.updateDistance;
+        const id = _id++;
         worlds[id] = this;
         Object.defineProperty(this, "id", {
             get: () => id
@@ -351,11 +441,22 @@
     World.getById = function (id) {
         return worlds[id];
     };
+    World.prototype.translate = function (x, y) {
+        this.translation.set(this.translation.add(new Vector2(x, y)));
+    };
+    World.prototype.translateScale = function (x, center = new Vector2(innerWidth / 2, innerHeight / 2)) {
+        if (this.scale + x <= 0) x = -this.scale / 10;// TODO: improvements on zooming back
+        this.scale += x;
+        this.translate(-center.x * x, -center.y * x);
+    };
     World.prototype.addTile = function (tile) {
         this.tiles.add(tile);
     };
     World.prototype.removeTile = function (tile) {
         this.tiles.delete(tile);
+    };
+    World.prototype.getMouseVector = function (v) {
+        return new Vector2((v.offsetX || v.x) - this.translation.x, (v.offsetY || v.y) - this.translation.y).scale(1 / this.scale);
     };
 
     function Animator(...fn) {
@@ -363,6 +464,7 @@
         fn = fn[0];
         if (!Array.isArray(fn)) fn = [fn];
         fn = new Set(fn);
+        let _fps = [];
         let frameId = null;
         let timeScale = 1.0;
         const animate = async (repeat = true) => {
@@ -375,11 +477,14 @@
                     r();
                 }));
             }
+            _fps.push(Date.now());
+            _fps = _fps.filter(i => i > Date.now() - 1000);
             if (repeat) await animate();
         };
         animate();
         return {
             getFrameId: () => frameId,
+            getFPS: () => _fps.length,
             stop: () => _cancelAnimationFrame(frameId),
             start: () => {
                 _cancelAnimationFrame(frameId);
@@ -430,6 +535,8 @@
     function MouseConstraint(world) {
         if (!isWeb) throw new Error("You can't use MouseConstraint method on a non-web platform.");
         if (!world) return console.warn("No world was given for MouseConstraint.");
+        const id = _id++;
+        world.mouseConstraints = [...(world.mouseConstraints || []), id];
         const el = world.canvas;
         const listeners = [];
         const l = (e, n, f) => {
@@ -437,11 +544,10 @@
             e.addEventListener(n, f);
         };
         let down = false;
-        let draggingTile = null;
-        let draggingGravity = null;
+        let drag = null;
         l(el, "mousedown", ev => {
             down = true;
-            const vec = new Vector2(ev.clientX, ev.clientY);
+            const vec = world.getMouseVector(ev);
             const tiles = Array.from(world.tiles);
             let tile;
             for (let i = 0; i < tiles.length; i++) {
@@ -452,28 +558,73 @@
                 }
             }
             if (tile) {
-                draggingTile = tile;
-                draggingGravity = tile.gravityEnabled;
+                if (drag && drag.tile) drag.tile.gravityEnabled = drag.gravity;
+                drag = {tile, gravity: tile.gravityEnabled, mouse: vec, tileVec: tile.clone()};
                 tile.gravityEnabled = false;
             }
         });
         l(el, "mousemove", ev => {
-            if (!down || !draggingTile) return;
-            const vec = new Vector2(ev.clientX, ev.clientY);
-            draggingTile.set(vec);
-            draggingTile.velocity.set(Vector2.zero());
+            if (!down || !drag) return;
+            const vec = world.getMouseVector(ev);
+            drag.tile.set(drag.tileVec.add(vec.sub(drag.mouse)));
+            drag.tile.resetVelocities();
         });
         l(el, "mouseup", () => {
             down = false;
-            if (draggingTile) draggingTile.gravityEnabled = draggingGravity;
-            draggingTile = null;
+            if (drag) drag.tile.gravityEnabled = drag.gravity;
+            drag = null;
         });
         l(window, "blur", () => {
             down = false;
-            if (draggingTile) draggingTile.gravityEnabled = draggingGravity;
-            draggingTile = null;
+            if (drag) drag.tile.gravityEnabled = drag.gravity;
+            drag = null;
         });
-        return {remove: () => listeners.forEach(i => i[0].removeEventListener(...i.slice(1)))};
+        return {
+            remove: () => {
+                listeners.forEach(i => i[0].removeEventListener(...i.slice(1)));
+                world.mouseConstraints = (world.mouseConstraints || []).filter(i => i !== id);
+            }
+        };
+    }
+
+    function CameraMover(world, options) {
+        if (!isWeb) throw new Error("You can't use MouseConstraint method on a non-web platform.");
+        if (!world) return console.warn("No world was given for MouseConstraint.");
+        const id = _id++;
+        const el = world.canvas;
+        options = processOptions(options, {
+            zoom: true,
+            move: true
+        });
+        world.cameraMovers = [...(world.cameraMovers || []), id];
+        const listeners = [];
+        const l = (e, n, f) => {
+            listeners.push(e, n, f);
+            e.addEventListener(n, f);
+        };
+        if (options.zoom) l(el, "wheel", ev => {
+            world.translateScale(-ev.deltaY / 5000, new Vector2(ev.offsetX, ev.offsetY));
+        });
+        if (options.move) {
+            let moving = false;
+            l(el, "mousedown", ev => {
+                const vec = world.getMouseVector(ev);
+                if (world.mouseConstraints.length > 0 && !Array.from(world.tiles).some(i => i.collidesWith(vec)))
+                    moving = true;
+            });
+            l(el, "mousemove", ev => {
+                if (!moving) return;
+                world.translate(ev.movementX, ev.movementY);
+            });
+            l(el, "mouseup", () => moving = false);
+            l(window, "blur", () => moving = false);
+        }
+        return {
+            remove: () => {
+                listeners.forEach(i => i[0].removeEventListener(...i.slice(1)));
+                world.cameraMovers = (world.cameraMovers || []).filter(i => i !== id);
+            }
+        };
     }
 
     extendClass(Rectangle, Polygon);
@@ -484,7 +635,8 @@
         Shapes: {Circle, Polygon, Rectangle},
         Tile,
         World,
-        Utils: {Animator, CanvasResizer, centerCanvas, MouseConstraint}
+        Utils: {Animator, CanvasResizer, centerCanvas, MouseConstraint, CameraMover},
+        DevUtils: {processOptions, extendClass}
     };
     _p(_exports.Phygic);
 })();
